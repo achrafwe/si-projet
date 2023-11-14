b99dcadec1fe0b358f2487a1532e6273bfc33133
achrafwe <106319432+achrafwe@users.noreply.github.com>
2023-11-14 23:28:02 +0100

    reponsive header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,15 +3,17 @@ import React from 'react';
 import logo from '../logo.svg';
 import logo1 from '../logo1.svg';
 
+
 function Header() {
   return (
     <nav>
-      <div className="main">
+      <div className="main" >
     <img className="logo-left" src="https://via.placeholder.com/267x97" />
     <img className="logo-right" src="https://via.placeholder.com/267x97" />
 </div>
     </nav>
   );
 }
+
 
 export default Header;
