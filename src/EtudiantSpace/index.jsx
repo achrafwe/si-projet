25f7cedba28697cf5f11659a17e9f1d3ac5fd318
achrafwe <106319432+achrafwe@users.noreply.github.com>
2023-12-18 18:07:36 +0100

    page stdent

--- src/EtudiantSpace/index.jsx
+++ src/EtudiantSpace/index.jsx
@@ -10,33 +10,33 @@ import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
 import './index.css';
 class EtudiantSpace extends Component{
-  changeColor(clickedButtonId) {
-    const buttons = ['button1', 'button2'];
-
-    buttons.forEach((buttonId) => {
-      const button = document.getElementById(buttonId);
-      if (buttonId === clickedButtonId) {
-        button.style.backgroundColor = '#DB4C3B';
-      } else {
-        button.style.backgroundColor = '#bfbfbf';
+    constructor(props) {
+      super(props);
+      this.state = {
+        searchValue: '',
+        showBottomSection: false,
+      };
+    }
+  
+    handleSearch = () => {
+      const { searchValue } = this.state;
+      if (searchValue.trim() !== '') {
+        this.setState({ showBottomSection: true });
+        // Vous pouvez ajouter ici le code de recherche ou redirection si nécessaire.
       }
-    });
-  }
-
-  render(){
-        return(
+    };
+  
+    handleChange = (event) => {
+      this.setState({ searchValue: event.target.value });
+    };
+  
+    render() {
+      const { showBottomSection, searchValue } = this.state;
+  
+      return (
             <div className="frame">
             <div className="div">
-              <div className="overlap">
-                <input className="text-wrapper" placeholder='Je cherche ......'/>
-                <IconButton>
-                <img
-                  className="icon-search"
-                  alt="Icon search"
-                  src="https://c.animaapp.com/Zzsi4ZsV/img/---icon--search-@2x.png"
-                />
-                </IconButton>
-              </div>
+            
               <img
                 className="img"
                 alt="User profile PNG"
@@ -46,44 +46,51 @@ class EtudiantSpace extends Component{
                 <div className="text-wrapper-11">Bienvenue Nom Etudiant</div>
               </div>
              
-              <div className="overlap-3">
-                <p className="t-l">
-                  <span className="span">tél :</span>
-                  <a href="tel:+0244022122" rel="noopener noreferrer" target="_blank">
-                    <span className="text-wrapper-4">0244022122</span>
-                  </a>
-                </p>
-                <div className="text-wrapper-5">Mr.May Madeth</div>
-                <img
-                  className="user-profile-PNG"
-                  alt="User profile PNG"
-                  src="https://c.animaapp.com/Zzsi4ZsV/img/user-profile-png-picture-2@2x.png"
-                />
-                <div className="mail" >mail: madeth.may@univ-lemans.fr</div>
-                <div className="overlap-4">
-                  <Button  className="text-wrapper-6" >Contactez directement</Button >
-                </div>
+              <div className="overlap">
+            <input
+              className="text-wrapper"
+              placeholder="Je cherche ......"
+              value={searchValue}
+              onChange={this.handleChange}
+            />
+            <IconButton onClick={this.handleSearch}>
+              <img
+                className="icon-search"
+                alt="Icon search"
+                src="https://c.animaapp.com/Zzsi4ZsV/img/---icon--search-@2x.png"
+              />
+            </IconButton>
+          </div>
+          {showBottomSection && (
+
+              <><div className="overlap-3">
+              <p className="t-l">
+                <span className="span">tél :</span>
+                <a href="tel:+0244022122" rel="noopener noreferrer" target="_blank">
+                  <span className="text-wrapper-4">0244022122</span>
+                </a>
+              </p>
+              <div className="text-wrapper-5">Mr.May Madeth</div>
+              <img
+                className="user-profile-PNG"
+                alt="User profile PNG"
+                src="https://c.animaapp.com/Zzsi4ZsV/img/user-profile-png-picture-2@2x.png" />
+              <div className="mail">mail: madeth.may@univ-lemans.fr</div>
+              <div className="overlap-4">
+                <Button className="text-wrapper-6">Contactez directement</Button>
               </div>
-              <div className="overlap-group">
-              <img className="vector" alt="Vector" src="https://c.animaapp.com/Zzsi4ZsV/img/vector-1.png" />
+            </div><div className="overlap-group">
+                <img className="vector" alt="Vector" src="https://c.animaapp.com/Zzsi4ZsV/img/vector-1.png" />
                 <button className="group" id="button1" onClick={() => this.changeColor('button1')}>
-                    <div className="text-wrapper-2">Etage 1</div>
+                  <div className="text-wrapper-2">Etage 1</div>
                 </button>
                 <button className="overlap-wrapper" id="button2" onClick={() => this.changeColor('button2')}>
-                    <div className="text-wrapper-3">RDC</div>
+                  <div className="text-wrapper-3">RDC</div>
                 </button>
-              </div>
-              <div className="ellipse" />
-              <div className="ellipse-2" />
-              <div className="ellipse-3" />
-           
-              <div className="text-wrapper-7">Disponible</div>
-              <div className="text-wrapper-8">Non diponible</div>
-              <div className="text-wrapper-9">Non spécifié</div>
-              <div className="text-wrapper-10">Actualité</div>
-            
+              </div><div className="ellipse" /><div className="ellipse-2" /><div className="ellipse-3" /><div className="text-wrapper-7">Disponible</div><div className="text-wrapper-8">Non diponible</div><div className="text-wrapper-9">Non spécifié</div><div className="text-wrapper-10">Actualité</div></>
+              )}
             </div>
-          </div>
+            </div>
         )
     }
 }
