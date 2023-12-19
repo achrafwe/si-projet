a56b5f4dd4222844672244fac9b6c4fd13f8fa1e
tahabouiber <t.bouiber@gmail.com>
2023-12-19 09:39:45 +0100

    bla bla bla

--- src/EtudiantSpace/index.jsx
+++ src/EtudiantSpace/index.jsx
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
-import SearchIcon from '@mui/icons-material/Search';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import InputBase from '@mui/material/InputBase';
 import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
 import './index.css';
+import './index.css';
+
+
 class EtudiantSpace extends Component{
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentSlide: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.startSlider();
+  }
+
+  componentWillUnmount() {
+    this.stopSlider();
+  }
+
+  startSlider = () => {
+    this.sliderInterval = setInterval(() => {
+      this.nextSlide();
+    }, 2000); // Change the duration as needed (in milliseconds)
+  };
+
+  stopSlider = () => {
+    clearInterval(this.sliderInterval);
+  };
+
+  nextSlide = () => {
+    this.setState((prevState) => ({
+      currentSlide: (prevState.currentSlide + 1) % slideImages.length,
+    }));
+  };
+
   changeColor(clickedButtonId) {
     const buttons = ['button1', 'button2'];
 
@@ -23,7 +51,9 @@ class EtudiantSpace extends Component{
     });
   }
 
+
   render(){
+
         return(
             <div className="frame">
             <div className="div">
@@ -82,9 +112,25 @@ class EtudiantSpace extends Component{
               <div className="text-wrapper-9">Non spécifié</div>
               <div className="text-wrapper-10">Actualité</div>
             
+              <div className="slider-container" onMouseEnter={this.stopSlider} onMouseLeave={this.startSlider}>
+            {slideImages.map((imageUrl, index) => (
+              <div key={index} className={index === this.state.currentSlide ? 'slide active' : 'slide'}>
+                <img src={imageUrl} alt={`Slide ${index + 1}`} />
+              </div>
+            ))}
+          </div>
+
             </div>
           </div>
         )
     }
 }
+
+const slideImages = [
+  'https://www.lafrap.fr/wp-content/uploads/2019/02/logo24h.png',
+  'https://cdn.worldvectorlogo.com/logos/ensim.svg'
+  // Add more image URLs as needed
+];
+
+
 export default EtudiantSpace;
